9c2da182f487abf9fd2f62329653f0013c2614c7
zoltan.zvara <zoltan.zvara@gmail.com>
2021-03-16 11:18:03 +0100

    Add Dockerfile and build.

--- docker/build-and-push.sh
+++ docker/build-and-push.sh
@@ -1,17 +1,63 @@
 #!/bin/bash
 
-# This helps on Bamboo to have actual script outputs.
-set -x
+set -x -o errexit -o pipefail -o noclobber -o nounset
+
+! getopt --test > /dev/null
+if [[ ${PIPESTATUS[0]} -ne 4 ]]; then
+    echo 'I’m sorry, `getopt --test` failed in this environment!'
+    exit 1
+fi
+
+SHORT_OPTIONS=m
+LONG_OPTIONS=module:
+
+! PARSED=$(getopt --options=$SHORT_OPTIONS --longoptions=$LONG_OPTIONS --name "$0" -- "$@")
+if [[ ${PIPESTATUS[0]} -ne 0 ]]; then
+    exit 2
+fi
+
+eval set -- "$PARSED"
+
+module=-
+while true; do
+    case "$1" in
+        -m|--module)
+            module="$2"
+            shift 2
+            ;;
+        --)
+            shift
+            break
+            ;;
+        *)
+            echo "Programming error!"
+            exit 3
+            ;;
+    esac
+done
+
+cp version.sbt version
+sed -i 's/version\ in\ ThisBuild\ \:\=\ \"//g' version
+sed -i 's/\"//g' version
 
 export BUILD_VERSION=$(<version)
-export NAMESPACE="spark"
-export APPLICATION="disqus"
-export REGISTRY="registry.sztaki.dev:443"
+export NAMESPACE="zzvara"
+export APPLICATION="spark-disqus"
+export BUILD_DIRECTORY="docker"
+export REGISTRY="registry.hub.docker.com"
 
-docker build -t $NAMESPACE/$APPLICATION:$BUILD_VERSION -t $NAMESPACE/$APPLICATION:latest -f $APPLICATION/src/main/resources/Dockerfile $APPLICATION/target/scala-2.12/
+cp -r $module/target/scala-2.12/$module-assembly-$BUILD_VERSION.jar $BUILD_DIRECTORY/$module-assembly-$BUILD_VERSION.jar || exit 1
 
-docker tag $NAMESPACE/$APPLICATION:$BUILD_VERSION $REGISTRY/$NAMESPACE/$APPLICATION:$BUILD_VERSION
-docker tag $NAMESPACE/$APPLICATION:latest $REGISTRY/$NAMESPACE/$APPLICATION:latest
+docker build --pull \
+             -t $NAMESPACE/$APPLICATION:$BUILD_VERSION \
+             -t $NAMESPACE/$APPLICATION:latest \
+             -f $BUILD_DIRECTORY/Dockerfile \
+             --build-arg module=$module \
+             --build-arg version=$BUILD_VERSION \
+             $BUILD_DIRECTORY || exit 1
 
-docker push $REGISTRY/$NAMESPACE/$APPLICATION:$BUILD_VERSION
-docker push $REGISTRY/$NAMESPACE/$APPLICATION:latest
+docker tag $NAMESPACE/$APPLICATION:$BUILD_VERSION $REGISTRY/$NAMESPACE/$APPLICATION:$BUILD_VERSION || exit 1
+docker tag $NAMESPACE/$APPLICATION:latest $REGISTRY/$NAMESPACE/$APPLICATION:latest || exit 1
+
+docker push $REGISTRY/$NAMESPACE/$APPLICATION:$BUILD_VERSION || exit 1
+docker push $REGISTRY/$NAMESPACE/$APPLICATION:latest || exit 1
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,11 +1,11 @@
 import sbt._
 
 object Dependencies {
-  val currentScalaVersion = "2.12.13"
+  val currentScalaVersion = "2.12.12"
 
   val scalaLanguage = "org.scala-lang" % "scala-library" % currentScalaVersion
   val scalaCompiler = "org.scala-lang" % "scala-compiler" % currentScalaVersion
-  val scalaTest = "org.scalatest" %% "scalatest" % "3.2.3" % "test"
+  val scalaTest = "org.scalatest" %% "scalatest" % "3.3.0-SNAP3" % "test"
 
   val coreDependencies = Seq(
     scalaLanguage,
